6b826a41ca272d0c9097659cc812ece715fe42b6
SillentTroll <guzun.alex.c@gmail.com>
2015-10-26 22:25:44 +0000

    added travis-ci.org build file

--- wsgi/config/config.py
+++ wsgi/config/config.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 import os
 
-
 PROPAGATE_EXCEPTIONS = True
 SECRET_KEY = os.urandom(24)
 API_PREFIX = "/api/v1/"
@@ -17,10 +16,13 @@ MONGODB_HOST = "db"
 MONGODB_PORT = 27017
 MONGO_URI = "mongodb://{0}:{1}/{2}".format(MONGODB_HOST, MONGODB_PORT, MONGODB_DB)
 
-# Redis
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
 
 # SECURITY
 JWT_AUTH_URL_RULE = API_PREFIX + 'users/auth'
 JWT_EXPIRATION_DELTA = timedelta(minutes=60)
+
+# Slack config
+SLACK_NOTIFY_HOOK_CONFIG = "https://hooks.slack.com/services/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"
+SLACK_TEAM_ID = "T0562HCT2"
+SLACK_START_COMMAND_TOKEN = "526LAYdxqLctuDlV4FWYta31"
+SLACK_STOP_COMMAND_TOKEN = "cFA6Nd3YEsm5uzWCYwjzl2wG"
